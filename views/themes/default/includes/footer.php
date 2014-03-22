e7b615912b6af9d8da7f22fcc3890865625009d3
serakfalcon <jrforjc@gmail.com>
2014-03-22 11:34:12 +0800

    re-adding bootstrap

--- views/themes/default/includes/footer.php
+++ views/themes/default/includes/footer.php
@@ -4,7 +4,7 @@
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="includes/bootstrap/js/bootstrap.min.js"></script>
+<script src="includes/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Table sorter plugin -->
 <script src="includes/DataTables/media/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript">
